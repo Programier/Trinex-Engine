99ec124d172f038ac10e8e6742ca1e23dff349c4
programier <wladnet28@gmail.com>
2022-03-28 18:40:58 +0300

    Update: Added freetype binaries for windows

--- build.bat
+++ build.bat
@@ -11,8 +11,8 @@ make -j%NUMBER_OF_PROCESSOR%
 
 copy ..\src\Engine\libs\glew32.dll .\glew32.dll
 copy ..\src\Engine\libs\libassimp-5.dll .\libassimp-5.dll
-copy ..\src\Engine\libs\libglfw.a .\libglfw.a
-copy ..\src\Engine\libs\glfw.dll .\glfw.dll
+copy ..\src\Engine\libs\glfw.dll .\glfw3.dll
+copy ..\src\Engine\libs\freetype.dll .\freetype.dll
 
 copy src\Engine\libGraphics.dll .\libGraphics.dll
 copy src\Engine\libImage.dll .\libImage.dll
