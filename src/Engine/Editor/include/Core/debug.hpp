23796bd9942a805e990f7ac0e30e0f1babf75ae6
programier <wladnet28@gmail.com>
2022-10-23 19:27:27 +0300

    Removed headers from Editor subproject

--- src/Engine/Editor/include/Core/debug.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define DEBUG
-
-#if defined(DEBUG)
-#define DEBUG_CODE(code) code
-#else
-#define DEBUG_CODE(code)
-#endif
--- src/Engine/Editor/include/Event/update_events.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Core/export.hpp>
-
-
-namespace Engine::UpdateEvent
-{
-    ENGINE_EXPORT void poll_events();
-    ENGINE_EXPORT void wait_for_event();
-}
