b4d2cddaf43a237431ecd00ecbcff8aa92e1013a
Programier <wladnet28@gmail.com>
2023-05-13 23:11:02 +0300

    Added basic mesh classes

--- src/Engine/include/Graphics/mesh.hpp
+++ src/Engine/include/Graphics/mesh.hpp
@@ -1,89 +1,124 @@
 #pragma once
 #include <Core/buffer_types.hpp>
+#include <Core/constants.hpp>
 #include <Core/pointer.hpp>
+#include <Core/predef.hpp>
 #include <Graphics/pipeline_buffers.hpp>
 
 
 namespace Engine
 {
-
-    struct ENGINE_EXPORT MeshSemanticEntry {
-    public:
-        byte count;
+    struct MeshSemanticEntry {
+        byte count = 0;
 
     private:
-        byte _M_variable_size;
+        byte _M_size = 0;
 
     public:
         template<typename Type>
-        static MeshSemanticEntry create()
+        static MeshSemanticEntry semantic_of()
         {
-            MeshSemanticEntry entry;
-            entry._M_variable_size = sizeof(Type);
-            return entry;
+            MeshSemanticEntry result;
+            result.count   = 0;
+            result._M_size = static_cast<byte>(sizeof(Type));
+            return result;
+        }
+
+        FORCE_INLINE size_t type_size() const
+        {
+            return static_cast<size_t>(_M_size);
+        }
+
+        FORCE_INLINE size_t semantic_size() const
+        {
+            return type_size() * static_cast<size_t>(count);
+        }
+
+        FORCE_INLINE size_t offset(byte index) const
+        {
+            if (count <= index)
+                return Constants::index_none;
+
+            return _M_size * static_cast<size_t>(index);
         }
     };
 
 
-    struct ENGINE_EXPORT MeshSemanticInfoBase {
-        virtual size_t semantic_offset(VertexBufferSemantic semantic, byte index) = 0;
+    class ENGINE_EXPORT MeshSemanticInfo
+    {
+    public:
+        virtual size_t semantic_offset(VertexBufferSemantic semantic, byte index = 0) const = 0;
     };
 
-
-    struct ENGINE_EXPORT StaticMeshSemanticInfo : public MeshSemanticInfoBase {
+    struct ENGINE_EXPORT StaticMeshSemanticInfo : public MeshSemanticInfo {
         union
         {
             struct {
                 MeshSemanticEntry vertex;
-                MeshSemanticEntry texcoord;
+                MeshSemanticEntry text_coord;
+                MeshSemanticEntry color;
+                MeshSemanticEntry normal;
+                MeshSemanticEntry tangent;
+                MeshSemanticEntry binormal;
+            } names_entries;
+
+            MeshSemanticEntry entries[6];
+        };
+
+        StaticMeshSemanticInfo();
+        size_t semantic_offset(VertexBufferSemantic semantic, byte index = 0) const override;
+    };
+
+    struct ENGINE_EXPORT DynamicMeshSemanticInfo : public MeshSemanticInfo {
+        union
+        {
+            struct {
+                MeshSemanticEntry vertex;
+                MeshSemanticEntry text_coord;
                 MeshSemanticEntry color;
                 MeshSemanticEntry normal;
                 MeshSemanticEntry tangent;
                 MeshSemanticEntry binormal;
                 MeshSemanticEntry blend_weight;
                 MeshSemanticEntry blend_indices;
-            };
+            } names_entries;
 
             MeshSemanticEntry entries[8];
         };
 
-        virtual size_t semantic_offset(VertexBufferSemantic semantic, byte index) override;
+        DynamicMeshSemanticInfo();
+        size_t semantic_offset(VertexBufferSemantic semantic, byte index = 0) const override;
     };
 
 
-    class ENGINE_EXPORT MeshSemanticsInfo : public SerializableObject
-    {
-        //        union
-        //        {
-        //            struct {
-        //                MeshSemanticEntry vertex(VertexBufferSemantic::Vertex);
-        //                MeshSemanticEntry texcoord;
-        //                MeshSemanticEntry color;
-        //                MeshSemanticEntry normal;
-        //                MeshSemanticEntry tangent;
-        //                MeshSemanticEntry binormal;
-        //                MeshSemanticEntry blend_weight;
-        //                MeshSemanticEntry blend_indices;
-        //            };
-
-        //            MeshSemanticEntry entries[8];
-        //        };
-
-       //bool serialize(BufferWriter* writer) override;
-        //bool deserialize(BufferReader* reader) override;
-    };
-
-
-    class ENGINE_EXPORT StaticMesh : public Object
+    class ENGINE_EXPORT Mesh : public Object
     {
     public:
         struct ENGINE_EXPORT MeshLOD {
             Pointer<VertexBuffer> vertex_buffer;
-            Vector<Pointer<IndexBuffer>> index_buffers;
+            Pointer<IndexBuffer> index_buffer;
         };
 
-    public:
-        MeshSemanticsInfo semantics_info;
         Vector<MeshLOD> lods;
+        virtual const MeshSemanticInfo& semantic_info() const = 0;
+    };
+
+
+    class ENGINE_EXPORT StaticMesh : public Mesh
+    {
+    public:
+        StaticMeshSemanticInfo info;
+        Vector<MeshLOD> lods;
+
+        const MeshSemanticInfo& semantic_info() const override;
+    };
+
+    class ENGINE_EXPORT DynamicMesh : public Mesh
+    {
+    public:
+        DynamicMeshSemanticInfo info;
+        Vector<MeshLOD> lods;
+
+        const MeshSemanticInfo& semantic_info() const override;
     };
 }// namespace Engine
--- src/Engine/src/Graphics/mesh.cpp
+++ src/Engine/src/Graphics/mesh.cpp
@@ -3,22 +3,67 @@
 #include <Core/engine.hpp>
 #include <Core/logger.hpp>
 #include <Graphics/mesh.hpp>
+#include <numeric>
 
 namespace Engine
 {
-    static const Array<size_t, 8> semantic_sizes = {
-            sizeof(Point3D),    // Vertex
-            sizeof(Point2D),    // TexCoord
-            sizeof(Vector4D),   // Color
-            sizeof(Vector3D),   // Normal
-            sizeof(Vector3D),   // Tangent
-            sizeof(Vector3D),   // Binormal,
-            sizeof(Vector4D),   // BlendWeight
-            sizeof(IntVector4D),// BlendIndices
-    };
-
-    size_t StaticMeshSemanticInfo::semantic_offset(VertexBufferSemantic semantic, byte index)
+    static size_t semantic_size_predicate(size_t last, const MeshSemanticEntry& entry)
     {
-        return 0;
+        return static_cast<size_t>(entry.count) + last;
+    }
+
+    StaticMeshSemanticInfo::StaticMeshSemanticInfo()
+    {
+        names_entries.vertex     = MeshSemanticEntry::semantic_of<Vector3D>();
+        names_entries.text_coord = MeshSemanticEntry::semantic_of<Vector2D>();
+        names_entries.color      = MeshSemanticEntry::semantic_of<Vector4D>();
+        names_entries.normal     = MeshSemanticEntry::semantic_of<Vector3D>();
+        names_entries.tangent    = MeshSemanticEntry::semantic_of<Vector3D>();
+        names_entries.binormal   = MeshSemanticEntry::semantic_of<Vector3D>();
+    }
+
+    DynamicMeshSemanticInfo::DynamicMeshSemanticInfo()
+    {
+        names_entries.vertex        = MeshSemanticEntry::semantic_of<Vector3D>();
+        names_entries.text_coord    = MeshSemanticEntry::semantic_of<Vector2D>();
+        names_entries.color         = MeshSemanticEntry::semantic_of<Vector4D>();
+        names_entries.normal        = MeshSemanticEntry::semantic_of<Vector3D>();
+        names_entries.tangent       = MeshSemanticEntry::semantic_of<Vector3D>();
+        names_entries.binormal      = MeshSemanticEntry::semantic_of<Vector3D>();
+        names_entries.blend_weight  = MeshSemanticEntry::semantic_of<Vector4D>();
+        names_entries.blend_indices = MeshSemanticEntry::semantic_of<IntVector4D>();
+    }
+
+
+    template<size_t size>
+    static size_t calculate_semantic_offset(const MeshSemanticEntry entries[size], VertexBufferSemantic semantic,
+                                            byte index)
+    {
+        Index semantic_index = static_cast<Index>(semantic);
+        if (semantic_index >= size || entries[semantic_index].count <= index)
+            return Constants::index_none;
+
+        return std::accumulate(entries, entries + semantic_index, 0, semantic_size_predicate) +
+               entries[semantic_index].offset(index);
+    }
+
+    size_t StaticMeshSemanticInfo::semantic_offset(VertexBufferSemantic semantic, byte index) const
+    {
+        return calculate_semantic_offset<sizeof(entries) / sizeof(MeshSemanticEntry)>(entries, semantic, index);
+    }
+
+    size_t DynamicMeshSemanticInfo::semantic_offset(VertexBufferSemantic semantic, byte index) const
+    {
+        return calculate_semantic_offset<sizeof(entries) / sizeof(MeshSemanticEntry)>(entries, semantic, index);
+    }
+
+    const MeshSemanticInfo& StaticMesh::semantic_info() const
+    {
+        return info;
+    }
+
+    const MeshSemanticInfo& DynamicMesh::semantic_info() const
+    {
+        return info;
     }
 }// namespace Engine
