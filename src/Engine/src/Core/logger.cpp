b784e9641234b2cba9eca3b2dd2b500f99b0a0f8
Programier <wladnet28@gmail.com>
2023-05-27 23:30:57 +0300

    Updated logger

--- src/Engine/src/Core/logger.cpp
+++ src/Engine/src/Core/logger.cpp
@@ -1,13 +1,42 @@
 #include <Core/logger.hpp>
 #include <Core/predef.hpp>
-#include <SDL_log.h>
 #include <cstdarg>
 #include <cstdio>
+#include <cstring>
 #include <cwchar>
+
+#if PLATFORM_WINDOWS
+#include <windows.h>
+#define RESET_COLOR (FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE)
+#define RED (FOREGROUND_RED)
+#define GREEN (FOREGROUND_GREEN)
+#define BLUE (FOREGROUND_BLUE)
+using ConsoleColor = DWORD;
+
+static void set_output_color(WORD color, FILE*)
+{
+    static HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(console, color);
+}
+
+#else
+
+#define RESET_COLOR "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define BLUE "\033[34m"
+
+using ConsoleColor = const char*;
+
+static void set_output_color(const char* color, FILE* output)
+{
+    fprintf(output, "%s", color);
+}
+
+#endif
 
 namespace Engine
 {
-
     Logger& Logger::log(const char* format, ...)
     {
         return *this;
@@ -35,17 +64,43 @@ namespace Engine
 
     class BasicLogger : public Logger
     {
+    private:
+        template<typename T, typename... Args>
+        bool or_operator(T first, Args... args)
+        {
+            return ((first == args) || ...);
+        }
+
+        void write_message(const char* tag, const char* format, va_list& args, FILE* out, ConsoleColor color)
+        {
+            char buffer[80];
+            std::time_t now = std::time(nullptr);
+            std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+
+            set_output_color(color, out);
+            fprintf(out, "[%6s][%s]: ", tag, buffer);
+
+            vfprintf(out, format, args);
+            set_output_color(RESET_COLOR, out);
+            fflush(out);
+
+            auto len = std::strlen(format);
+            while (len > 0 && or_operator(format[len - 1], '\0', ' ', '\t', '\r')) --len;
+            if (len > 0 && format[len - 1] != '\n')
+            {
+                fprintf(out, "\n");
+            }
+        }
+
+
     public:
         BasicLogger& log(const char* format, ...)
         {
-#ifdef TRINEX_ENGINE_DEBUG
             va_list args;
             va_start(args, format);
-            char buffer[1024];
-            vsprintf(buffer, format, args);
+            write_message("LOG", format, args, stdout, GREEN);
             va_end(args);
-            printf("%s\n", buffer);
-#endif
+
             return *this;
         }
 
@@ -54,10 +109,8 @@ namespace Engine
 #ifdef TRINEX_ENGINE_DEBUG
             va_list args;
             va_start(args, format);
-            char buffer[1024];
-            vsprintf(buffer, format, args);
+            write_message("DEBUG", format, args, stdout, GREEN);
             va_end(args);
-            printf("%s\n", buffer);
 #endif
             return *this;
         }
@@ -67,10 +120,8 @@ namespace Engine
 #ifdef TRINEX_ENGINE_DEBUG
             va_list args;
             va_start(args, format);
-            char buffer[1024];
-            vsprintf(buffer, format, args);
+            write_message("WARNING", format, args, stdout, BLUE);
             va_end(args);
-            printf("%s\n", buffer);
 #endif
             return *this;
         }
@@ -78,27 +129,15 @@ namespace Engine
 
         BasicLogger& error(const char* format, ...)
         {
-#ifdef TRINEX_ENGINE_DEBUG
             va_list args;
             va_start(args, format);
-            char buffer[1024];
-            vsprintf(buffer, format, args);
+            write_message("ERROR", format, args, stderr, RED);
             va_end(args);
-            printf("%s\n", buffer);
-#endif
             return *this;
         }
 
         BasicLogger& error(const String& msg, const MessageList& messages)
         {
-#ifdef TRINEX_ENGINE_DEBUG
-            printf("%s\n", msg.c_str());
-
-            for (auto& str : messages)
-            {
-                printf("\t%s", str.c_str());
-            }
-#endif
             return *this;
         }
     };
