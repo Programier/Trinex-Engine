41e484b0c77aff7c06be65946502718700159e34
Programier <44412389+Programier@users.noreply.github.com>
2025-10-14 01:36:31 +0300

    Added algorithm header

---
+++ include/Core/etl/algorithm.hpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+
+namespace Engine::etl
+{
+	using std::all_of;
+	using std::any_of;
+	using std::clamp;
+	using std::copy;
+	using std::copy_if;
+	using std::equal;
+	using std::find;
+	using std::find_if;
+	using std::for_each;
+	using std::lower_bound;
+	using std::max;
+	using std::min;
+	using std::none_of;
+	using std::remove;
+	using std::remove_if;
+	using std::sort;
+	using std::swap;
+	using std::transform;
+	using std::unique;
+	using std::upper_bound;
+}// namespace Engine::etl
